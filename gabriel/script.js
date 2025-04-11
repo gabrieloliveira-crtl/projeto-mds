e8c881eb9e0a60a7c3958749761ba9a10bab21df
Gabriel Oliveira <gabrieloliverasantos162@gmail.com>
2025-04-11 00:37:15 -0300

    save

---
+++ gabriel/script.js
@@ -0,0 +1,15 @@
+ScrollReveal().reveal('#texto',{
+    duration: 3000, // duração do efeito (em milisegundos), define a velocidade da transição
+    origin: 'left', // origem do efeito: o conteudo virá da direita para a esquerda
+    distance : '50px', // distancia que o elemento "viaja" antes de aparecer
+})
+ScrollReveal().reveal('.sect2',{
+    duration: 1000, // duração do efeito (em milisegundos), define a velocidade da transição
+    origin: 'left', // origem do efeito: o conteudo virá da direita para a esquerda
+    distance : '50px', // distancia que o elemento "viaja" antes de aparecer
+})
+ScrollReveal().reveal('.sect1',{
+    duration: 1000, // duração do efeito (em milisegundos), define a velocidade da transição
+    origin: 'bottom', // origem do efeito: o conteudo virá da direita para a esquerda
+    distance : '50px', // distancia que o elemento "viaja" antes de aparecer
+})
