9b186687e4fd2c208ffe1eec1ddbe7bec8ec5d12
Gabriel Oliveira <gabrieloliveirasantos162@gmail.com>
2025-04-16 20:12:46 -0300

    ld clksm

--- gabriel/script.js
+++ gabriel/script.js
@@ -13,3 +13,13 @@ ScrollReveal().reveal('.sect1',{
     origin: 'bottom', // origem do efeito: o conteudo virá da direita para a esquerda
     distance : '50px', // distancia que o elemento "viaja" antes de aparecer
 })
+ScrollReveal().reveal('#img',{
+    duration: 3000, // duração do efeito (em milisegundos), define a velocidade da transição
+    origin: 'left', // origem do efeito: o conteudo virá da direita para a esquerda
+    distance : '50px', // distancia que o elemento "viaja" antes de aparecer
+})
+
+
+
+
+
